da407f029af38d7771ec10e0489703702786abb1
QiDa <974830507@qq.com>
2019-04-14 22:17:19 +0800

    添加116点实体

---
+++ ReadEntialls/ReadEntiall_116.m
@@ -0,0 +1,27 @@
+function [res]=ReadEntiall_112(Pstr,Pvec,type,colorNo,formNo,transformationMatrixPtr)
+res.type=type;
+% 在国标P116
+res.name='点';
+res.name='POINT';
+
+res.p=Pvec(2:4)';
+
+res.x=res.p(1);
+res.y=res.p(2);
+res.z=res.p(3);
+
+try
+    res.ptr=round((Pvec(5)+1)/2);
+catch
+    res.ptr=0;
+end
+
+res.original=1;
+
+res.trnsfrmtnmtrx=transformationMatrixPtr;
+
+res.clrnmbr=colorNo;
+res.color=[0,0,0];
+
+res.well=true;
+end
