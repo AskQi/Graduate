0877f5f6fbc0a1d99a1a2e8780cb554add6180ab
QiDa <974830507@qq.com>
2019-04-18 15:10:29 +0800

    不再使用DrawingEntialls绘图

--- DrawingEntialls/DrawingEntiall_314.m
+++
@@ -1,5 +0,0 @@
-function [res]=DrawingEntiall_314(thisEntiall)
-fprintf('不处理颜色实体(314)\n');
-
-res=true;
-end
--- DrawingEntialls/DrawingEntiall_406.m
+++
@@ -1,5 +0,0 @@
-function [res]=DrawingEntiall_406(thisEntiall)
-fprintf('不处理该类型实体(406)\n');
-
-res=true;
-end
