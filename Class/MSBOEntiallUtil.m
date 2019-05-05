9119c671361e7c06a9698f7d6ebbace849c423c1
QiDa <974830507@qq.com>
2019-05-05 23:39:14 +0800

    实现B-Rep实体相关绘制

--- Class/MSBOEntiallUtil.m
+++ Class/MSBOEntiallUtil.m
@@ -28,10 +28,48 @@ classdef MSBOEntiallUtil
                     ParameterData{thisLoopEntiallIndex}.clrnmbr=clrnmbr;
                     
                     thisLoopEntiall=ParameterData{thisLoopEntiallIndex};
-                    thisLineEntiallStruct(:)=thisLoopEntiall.LineEntiall;
+                    thisLineAndEdgeEntiallStruct(:)=thisLoopEntiall.LineAndEdgeEntiall;
                     for jjj=1:thisLoopEntiall.n
-                        thisLineEntiallIndex=(thisLineEntiallStruct(jjj).CURV+1)/2;
-                        ParameterData{thisLineEntiallIndex}.clrnmbr=clrnmbr;
+                        curv=thisLineAndEdgeEntiallStruct(jjj).CURV;
+                        edge=thisLineAndEdgeEntiallStruct(jjj).edge;
+                        ndx=thisLineAndEdgeEntiallStruct(jjj).ndx;
+                        if ~isempty(curv)
+                            thisLineEntiallIndex=(curv+1)/2;
+                            ParameterData{thisLineEntiallIndex}.clrnmbr=clrnmbr;
+                        end
+                        if ~isempty(edge)
+                            thisEdgeEntiallIndex=(edge+1)/2;
+                            
+                            ParameterData{thisEdgeEntiallIndex}.clrnmbr=clrnmbr;
+                            
+                            thisEdgeEntiall=ParameterData{thisEdgeEntiallIndex};
+                            thisEdgeEntiallStruct(:)=thisEdgeEntiall.EdgeEntiall;
+                            for jjjj=1:thisEdgeEntiall.n
+                                % 曲线的DE指针
+                                curv=thisEdgeEntiallStruct(jjjj).curv;
+                                % 顶点列表实体（502）的DE指针
+                                svp=thisEdgeEntiallStruct(jjjj).svp;
+                                % 顶点列表实体第一个起始顶点的DE指针
+                                sv=thisEdgeEntiallStruct(jjjj).sv;
+                                % 顶点列表实体第一个终止顶点的DE指针
+                                tvp=thisEdgeEntiallStruct(jjjj).tvp;
+                                % 顶点列表实体中指定i的那的列表索引
+                                tv=thisEdgeEntiallStruct(jjjj).tv;
+                                
+                                thisCurvEntiallIndex=(curv+1)/2;
+                                ParameterData{thisCurvEntiallIndex}.clrnmbr=clrnmbr;
+                                % 顶点实体(502)
+                                thisSvpEntiallIndex=(svp+1)/2;
+                                ParameterData{thisSvpEntiallIndex}.clrnmbr=clrnmbr;
+
+                                thisSvEntiallIndex=(sv+1)/2;
+                                thisTvpEntiallIndex=(tvp+1)/2;
+                                thisTvEntiallIndex=(tv+1)/2;
+                                % 顶点实体结束
+                                
+                            end
+                        end
+                        
                     end
                 end
             end
