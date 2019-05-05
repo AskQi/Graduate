e5441612e3f34414aafcc378b09fc9f189ba5d34
QiDa <974830507@qq.com>
2019-05-05 11:35:06 +0800

    添加508实体的读取

---
+++ ReadEntialls/ReadEntiall_508.m
@@ -0,0 +1,28 @@
+function [res]=ReadEntiall_508(Pstr,Pvec,type,colorNo,formNo,transformationMatrixPtr)
+res.type=type;
+% 环实体，在国标P402
+
+res.name='环实体';
+res.original=1;
+res.superior=1;
+
+n=Pvec(2);
+res.n=n;
+
+point=2;
+
+for j=1:n
+   thisLoopEntity.type=Pvec(point+1);
+   thisLoopEntity.edge=Pvec(point+2);
+   thisLoopEntity.ndx=Pvec(point+3);
+   thisLoopEntity.of=Pvec(point+4);
+   k=Pvec(point+5);
+   thisLoopEntity.k=k;
+   thisLoopEntity.ISOP_CURV=Pvec(point+6:point+k*2+5);
+   point=point+k*2+5;
+   res.LoopEntity(j)=thisLoopEntity;
+end
+res.colorNo=colorNo;
+res.transformationMatrixPtr=transformationMatrixPtr;
+res.well=false;
+end
