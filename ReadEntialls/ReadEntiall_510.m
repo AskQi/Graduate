4fb000f5364136838204f6a6bb5db343de69989d
QiDa <974830507@qq.com>
2019-05-05 11:35:18 +0800

    添加510实体的读取

---
+++ ReadEntialls/ReadEntiall_510.m
@@ -0,0 +1,21 @@
+function [res]=ReadEntiall_510(Pstr,Pvec,type,colorNo,formNo,transformationMatrixPtr)
+res.type=type;
+% 面实体，在国标P403
+
+res.name='环实体';
+res.original=1;
+res.superior=1;
+
+res.surf=Pvec(2);
+
+n=Pvec(3);
+res.n=n;
+
+res.of=Pvec(4);
+
+res.loop=Pvec(5:n+4);
+
+res.colorNo=colorNo;
+res.transformationMatrixPtr=transformationMatrixPtr;
+res.well=false;
+end
