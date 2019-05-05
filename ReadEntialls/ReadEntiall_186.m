1d0a0a25b38e2216aa17d8ae0f1056771d90e4d0
QiDa <974830507@qq.com>
2019-05-05 11:34:03 +0800

    添加186实体的读取

---
+++ ReadEntialls/ReadEntiall_186.m
@@ -0,0 +1,26 @@
+function [res]=ReadEntiall_186(Pstr,Pvec,type,colorNo,formNo,transformationMatrixPtr)
+res.type=type;
+% 流形实体B-Rep对象实体，在国标P158
+
+res.name='流形实体B-Rep对象实体';
+res.original=1;
+res.superior=1;
+
+res.shell=Pvec(2);
+res.sof=Pvec(3);
+
+n=Pvec(4);
+res.n=n;
+
+point=4;
+for j=1:n
+   thisMSBOEntity.void=Pvec(point+1);
+   thisMSBOEntity.vof=Pvec(point+2);
+   point=point+2;
+   res.MSBOEntity(j)=thisMSBOEntity;
+end
+
+res.colorNo=colorNo;
+res.transformationMatrixPtr=transformationMatrixPtr;
+res.well=false;
+end
