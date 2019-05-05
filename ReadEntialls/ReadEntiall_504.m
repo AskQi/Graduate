c246f19a19c73f0ef9cb7225e007d84f5d125610
QiDa <974830507@qq.com>
2019-05-05 11:34:53 +0800

    添加504实体的读取

---
+++ ReadEntialls/ReadEntiall_504.m
@@ -0,0 +1,25 @@
+function [res]=ReadEntiall_504(Pstr,Pvec,type,colorNo,formNo,transformationMatrixPtr)
+res.type=type;
+% 边实体，在国标P400
+
+res.name='边实体';
+res.original=1;
+res.superior=1;
+
+n=Pvec(2);
+res.n=n;
+
+point=2;
+for j=1:n
+   thisEdgeEntity.curv=Pvec(point+1);
+   thisEdgeEntity.svp=Pvec(point+2);
+   thisEdgeEntity.tvp=Pvec(point+3);
+   thisEdgeEntity.tv=Pvec(point+4);
+   point=point+4;
+   res.EdgeEntity(j)=thisEdgeEntity;
+end
+res.formNo=formNo;
+res.colorNo=colorNo;
+res.transformationMatrixPtr=transformationMatrixPtr;
+res.well=false;
+end
